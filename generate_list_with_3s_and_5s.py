604cc37d17bd58bdf0e365b82349fd34fe27242b
Sravanti <63496387+Siddelasravanti@users.noreply.github.com>
2023-09-22 14:00:04 +0530

    Update generate_list_with_3s_and_5s.py

--- generate_list_with_3s_and_5s.py
+++ generate_list_with_3s_and_5s.py
@@ -1,6 +1,7 @@
 """
 Here we need to generate a list which contains  either 3s' or 5s'  existed in the given range.
 If the range is between 1-50 then list should be like below.
+expected output :
 l=[3,5,13,15,23,25,30,31,32.....43,45,50]
 here 50 is included
 """
